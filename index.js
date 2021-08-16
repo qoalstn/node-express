f90658cbf2d528b77727d276de96e6e386ebe62e
Seangki Kim <mousson2020@gmail.com>
2021-08-16 22:41:19 +0900

    create

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+//Middleware
+app.use(express.json());
+
+//Connect to DB
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to mongoDB!")
+);
+
+const authRoute = require("./routes/auth");
+
+//Route Middlewares
+app.use("/api/user", authRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello!");
+});
+
+app.listen(3333, () => console.log("3333 server start"));
