b89aa514fe8eeb8b0217e3645f092cab77dda583
Seangki Kim <mousson2020@gmail.com>
2021-08-31 22:25:43 +0900

    fix : apply jwt

--- service/auth.service.js
+++ service/auth.service.js
@@ -5,70 +5,163 @@ const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
-exports.join = async (req, res) => {
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist) {
-    return res.status(500).send("Email already exists");
-  }
-  //hash passwords
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  const client_token = String(Math.random());
-  const token = await bcrypt.hash(client_token, salt);
-  const today = new Date();
-  const tomorrow = new Date(today.setDate(today.getDate() + 1));
+// exports.join = async (req, res) => {
+//   const emailExist = await User.findOne({ email: req.body.email });
+//   if (emailExist) {
+//     return res.status(500).send("Email already exists");
+//   }
+//   //hash passwords
+//   const salt = await bcrypt.genSalt(10);
+//   const hashedPassword = await bcrypt.hash(req.body.password, salt);
+//   const client_token = String(Math.random());
+//   const token = await bcrypt.hash(client_token, salt);
+//   const today = new Date();
+//   const tomorrow = new Date(today.setDate(today.getDate() + 1));
 
-  const user = new User({
-    email: req.body.email,
-    password: hashedPassword,
-    content: req.body.content,
-    acs_token: token,
-    acs_exp: tomorrow,
+//   const user = new User({
+//     email: req.body.email,
+//     password: hashedPassword,
+//     content: req.body.content,
+//     acs_token: token,
+//     acs_exp: tomorrow,
+//   });
+
+//   try {
+//     await user.save();
+//     res.send({ user: user._id, access_token: client_token, acs_exp: tomorrow });
+//   } catch (err) {
+//     res.status(400).send(err);
+//   }
+// };
+
+// exports.login = async (req, res) => {
+//   const loginSalt = await bcrypt.genSalt(10);
+//   const hashedPassword = await bcrypt.hash(req.body.password, loginSalt);
+//   console.log(hashedPassword);
+
+//   const checkUser = await User.findOne({
+//     email: req.body.email,
+//   });
+//   console.log(checkUser);
+
+//   bcrypt.hash(req.body.password, 10, (err, hash) => {
+//     console.log("hash", hash);
+//     if (err) {
+//       throw err;
+//     }
+//     bcrypt.compare(req.body.password, hash, async (err, result) => {
+//       if (err) {
+//         return res.status(500).send("User is not found");
+//       }
+//       if (result) {
+//         const salt = await bcrypt.genSalt(10);
+//         const token = await bcrypt.hash(String(Math.random()), salt);
+
+//         const today = new Date();
+//         const tomorrow = new Date(today.setDate(today.getDate() + 1));
+
+//         User.updateOne(
+//           { email: req.body.email },
+//           { $set: { acs_token: token, acs_exp: tomorrow } }
+//         );
+
+//         res.json({ status: "00", token: token });
+//       }
+//     });
+//   });
+// };
+
+async function createToken(inputMail) {
+  const accessToken = jwt.sign({ id: inputMail }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "12h",
+    algorithm: "HS256",
   });
-
-  try {
-    await user.save();
-    res.send({ user: user._id, access_token: client_token, acs_exp: tomorrow });
-  } catch (err) {
-    res.status(400).send(err);
-  }
-};
-
-exports.login = async (req, res) => {
-  const loginSalt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, loginSalt);
-  console.log(hashedPassword);
-
-  const checkUser = await User.findOne({
-    email: req.body.email,
+  const refreshToken = jwt.sign({ id: inputMail }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "14d",
+    algorithm: "HS256",
   });
-  console.log(checkUser);
+  return { accessToken, refreshToken };
+}
 
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    console.log("hash", hash);
-    if (err) {
-      throw err;
-    }
-    bcrypt.compare(req.body.password, hash, async (err, result) => {
-      if (err) {
-        return res.status(500).send("User is not found");
+exports.join = async (registerType, req, res) => {
+  const reg_email =
+    /^[0-9a-zA-Z]([\-.\w]*[0-9a-zA-Z\-_+])*@([0-9a-zA-Z][\-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9}$/;
+  const reg_pass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/;
+  const inputMail = req.body.email;
+  const inputPass = req.body.password;
+
+  if (!inputMail || !inputPass) {
+    res.json({ status: "01", msg: "no input" });
+  } else if (!reg_email.test(inputMail)) {
+    res.json({ status: "02", msg: "email fomat error" });
+  } else if (!reg_pass.test(inputPass)) {
+    res.json({
+      status: "02",
+      msg: "password fomat error",
+    });
+  } else {
+    const data = await User.findOne({
+      email: req.body.email,
+    });
+    if (data.length != 0) {
+      res.json({ status: "02", msg: "email exist" });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(inputPass, salt);
+
+      //DB
+      try {
+        const user = new User({
+          email: req.body.email,
+          password: hashedPass,
+        });
+        await user.save();
+      } catch {
+        res.send(500).json({ msg: "join fail" });
       }
-      if (result) {
-        const salt = await bcrypt.genSalt(10);
-        const token = await bcrypt.hash(String(Math.random()), salt);
 
+      //token
+      const { accessToken } = await createToken(inputMail);
+      if (accessToken) {
         const today = new Date();
         const tomorrow = new Date(today.setDate(today.getDate() + 1));
 
         User.updateOne(
           { email: req.body.email },
-          { $set: { acs_token: token, acs_exp: tomorrow } }
+          { $set: { acs_token: accessToken, acs_exp: tomorrow } }
         );
 
-        res.json({ status: "00", token: token });
+        res.send(200).json({ accessToken: accessToken });
+      } else {
+        res.send(500).json({ msg: "token fail" });
       }
+    }
+  }
+};
+
+//
+exports.login = async (registerType, req, res) => {
+  const inputId = req.body.email;
+  const inputPass = req.body.password;
+  if (!inputId || !inputPass) {
+    return res.send(400).json({ msg: "no input" });
+  } else {
+    const data = await User.findOne({
+      email: inputId,
     });
-  });
+    const dbPass = data.length > 0 ? data[0].pass : "";
+    const validPass = await bcrypt.compare(inputPass, dbPass);
+    if (!validPass) {
+      return res.send(500).json({ msg: "login info error" });
+    }
+    //토큰 발행
+    else {
+      const { accessToken } = await createToken(inputId);
+      res.send(200).json({
+        accessToken: accessToken,
+      });
+    }
+  }
 };
 
 exports.getContens = async (req, res) => {
@@ -117,9 +210,6 @@ exports.authorizationToken = async (req, res, next) => {
       return res.status(500).send("Unavailable token");
     }
     bcrypt.compare(inputToken, hash, async (err, result) => {
-      console.log(hash);
-      console.log(checkUser.acs_token);
-      console.log(result);
       if (err) {
         return res.status(500).send("Unavailable token");
       }
